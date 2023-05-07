abc4bfe47d97b5a7395c871c78637c980fbd3472
Karen <kvelazquezfarina@gmail.com>
2023-05-07 20:50:03 -0300

    miprimerjs

---
+++ js/script.js
@@ -0,0 +1,14 @@
+console.log('Hola, soy la Calculadora y resolveré tus problemas...')
+
+let numeroUno = Number(prompt('Escribe un número'))
+let numeroDos = Number(prompt('Escribe un número'))
+
+let cuentaUno = numeroUno + numeroDos
+let cuentaDos = numeroUno - numeroDos
+let cuentaTres = numeroUno * numeroDos
+let cuentaCuatro = numeroUno / numeroDos
+
+console.log('Total suma = ' + cuentaUno)
+console.log('Total resta = ' + cuentaDos)
+console.log('Total multiplicación = ' + cuentaTres)
+console.log('Total división = ' + cuentaCuatro)
